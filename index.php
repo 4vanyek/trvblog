9f4961155f7b8d47748466f58863270ebb69b9f5
4vanyek <65071577+4vanyek@users.noreply.github.com>
2023-06-25 21:02:19 +0300

    a fix maybe?

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
     </section>
 
 </section>
-<script src="<?= asset('assets/js/jquery.min.js') ?>"></script>
-<script src="<?= asset('assets/js/bootstrap.min.js') ?>"></script>
+<script src="<?= asset('./assets/js/jquery.min.js') ?>"></script>
+<script src="<?= asset('./assets/js/bootstrap.min.js') ?>"></script>
 </body>
 </html>
